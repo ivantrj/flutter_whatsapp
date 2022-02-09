21c1876261d2e7fd6077f927ee11ea51513faf2f
Ivan Trj <ivantrajanovski@gmail.com>
2022-02-09 17:02:29 +0100

    add web and mobile responsivity

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp/colors.dart';
+import 'package:flutter_whatsapp/screens/mobile_screen_layout.dart';
+import 'package:flutter_whatsapp/screens/web_screen_layout.dart';
+import 'responsive/responsive_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
+    );
+  }
+}
