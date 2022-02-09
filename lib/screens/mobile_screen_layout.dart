820391029be785eb3d031ef0dcb14e8ec89109ae
Ivan Trj <ivantrajanovski@gmail.com>
2022-02-09 17:15:37 +0100

    add app bar

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp/colors.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -7,7 +8,55 @@ class MobileScreenLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
-      child: Scaffold(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: appBarColor,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+              color: Colors.grey,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(
+                text: 'Chats',
+              ),
+              Tab(
+                text: 'Status',
+              ),
+              Tab(
+                text: 'Calls',
+              )
+            ],
+          ),
+        ),
+        body: const Text("Mobile"),
+      ),
     );
   }
 }
