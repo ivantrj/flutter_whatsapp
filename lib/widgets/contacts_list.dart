991e264862f204ca767153bf9a48e9782d704b88
Ivan Trj <ivantrajanovski@gmail.com>
2022-02-09 17:21:43 +0100

    add contacts list

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../info.dart';
+
 class ContactsList extends StatelessWidget {
   const ContactsList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: ListView.builder(
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              info[index]['name'].toString(),
+              style: const TextStyle(fontSize: 18),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 6),
+              child: Text(
+                info[index]['message'].toString(),
+                style: const TextStyle(fontSize: 15),
+              ),
+            ),
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                info[index]['profilePic'].toString(),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
